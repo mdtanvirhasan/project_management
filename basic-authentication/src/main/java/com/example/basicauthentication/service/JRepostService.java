634881f5c22594ecddc85d13f3fbeccdd59d7f6e
Akid Tamjid Rahman <90531515+tamjid12@users.noreply.github.com>
2023-10-08 17:59:49 +0600

    jrxml file configuration left.

---
+++ basic-authentication/src/main/java/com/example/basicauthentication/service/JRepostService.java
@@ -0,0 +1,36 @@
+package com.example.basicauthentication.service;
+
+import com.example.basicauthentication.entity.Project;
+import com.example.basicauthentication.repository.ProjectRepository;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class JRepostService {
+
+    @Autowired
+    ProjectRepository projectRepository;
+    public void exportJasperReport(HttpServletResponse response) throws JRException, IOException {
+        List<Project> address = projectRepository.findAll();
+        //Get file and compile it
+        File file = ResourceUtils.getFile("classpath:Address_01.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(address);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Simplifying Tech");
+        //Fill Jasper report
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        //Export report
+        JasperExportManager.exportReportToPdfStream(jasperPrint,response.getOutputStream());
+    }
+}
